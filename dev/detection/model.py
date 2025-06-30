d38007c922a8926f5d9be1977bc0c2cfa3b6d14e
Oliver Yu <yuj1@livemail.uthscsa.edu>
2025-06-30 15:57:20 -0500

    added functionality for model version to be modifiable within the config file

--- dev/detection/model.py
+++ dev/detection/model.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 import torchvision
 
-def get_model(pretrained=False):
-    # Use DenseNet for patch classification
-    model = torchvision.models.densenet121(pretrained=pretrained)
-    model.features.conv0 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)  # single channel
+def get_model(arch="densenet121", pretrained=False):
+    model_fn = getattr(torchvision.models, arch)
+    model = model_fn(pretrained=pretrained)
+    model.features.conv0 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
     num_features = model.classifier.in_features
     model.classifier = nn.Sequential(
         nn.Linear(num_features, 1)
